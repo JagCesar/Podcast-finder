8b7ef55b6d43d5dab9f9237f43e138d52611b7a0
César Pinto Castillo <cesar@jagcesar.se>
2016-08-31 10:10:44 +0200

    First commit

---
+++ MessagesExtension/iTunes/ItunesSearch.swift
@@ -0,0 +1,15 @@
+//
+//  ItunesSearch.swift
+//  messageApplication
+//
+//  Created by Cesar Pinto Castillo on 2016-08-31.
+//  Copyright © 2016 JagCesar. All rights reserved.
+//
+
+import Foundation
+
+class ItunesSearch {
+    static func searchPodcast(term: String, completion:() -> Void) {
+
+    }
+}
